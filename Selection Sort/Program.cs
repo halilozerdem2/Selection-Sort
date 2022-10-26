65ad7d96c693880bba352985911fae2e69b95f80
lilahmedrezo <i.h.ozerdem@gmail.com>
2022-10-26 18:24:32 +0300

    Add project files.

---
+++ Selection Sort/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_Sort
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 7, 9, 16, 2, 31, 6, 97, 6, 101, 121 };
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+
+            SortTheListBySelection(numbers);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i]+ " ");
+            }
+            Console.ReadLine();
+
+        }
+
+        // Selection Sort
+        public static int[] SortTheListBySelection(int[] aNumbers)
+        {
+            int temp;
+
+            for (int i = 0; i < aNumbers.Length; i++)
+            {
+                for (int j = i+1; j < aNumbers.Length; j++)
+                {
+                    if (aNumbers[i] < aNumbers[j])
+                        continue;
+
+                    else
+                    {
+                        temp = aNumbers[i];
+                        aNumbers[i] = aNumbers[j];
+                        aNumbers[j] = temp;
+                    }
+                }
+            }
+            return aNumbers;
+        } 
+    }
+}
